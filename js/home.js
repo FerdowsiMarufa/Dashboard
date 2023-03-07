4fccd17c694a5e5a446181fff64e0a162306aa96
marufa <marufa.fm@gmail.com>
2023-03-07 14:22:26 +0600

    chart done

--- js/home.js
+++ js/home.js
@@ -1,45 +1,3 @@
-// var ctx = document.getElementById("myChart").getContext("2d");
-// var myChart = new Chart(ctx, {
-//   type: "bar",
-//   data: {
-//     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//     datasets: [
-//       {
-//         label: "# of Votes",
-//         data: [12, 19, 3, 5, 2, 3],
-//         backgroundColor: [
-//           "rgba(255, 99, 132, 0.2)",
-//           "rgba(54, 162, 235, 0.2)",
-//           "rgba(255, 206, 86, 0.2)",
-//           "rgba(75, 192, 192, 0.2)",
-//           "rgba(153, 102, 255, 0.2)",
-//           "rgba(255, 159, 64, 0.2)",
-//         ],
-//         borderColor: [
-//           "rgba(255,99,132,1)",
-//           "rgba(54, 162, 235, 1)",
-//           "rgba(255, 206, 86, 1)",
-//           "rgba(75, 192, 192, 1)",
-//           "rgba(153, 102, 255, 1)",
-//           "rgba(255, 159, 64, 1)",
-//         ],
-//         borderWidth: 1,
-//       },
-//     ],
-//   },
-//   options: {
-//     scales: {
-//       yAxes: [
-//         {
-//           ticks: {
-//             beginAtZero: true,
-//           },
-//         },
-//       ],
-//     },
-//   },
-// });
-
 const dataArray = [];
 fetch("http://localhost:81/GetAllMenuList")
   .then((response) => response.json())
@@ -58,74 +16,229 @@ fetch("http://localhost:81/GetAllMenuList")
     });
     // done();
     displaydata();
-    // document.querySelector(".menu-approval").innerHTML = dataArray[0].menuName;
-    // document.querySelector(".report-menu").innerHTML = dataArray[3].menuName;
   });
-// let sp = document.createElement("p");
-let approvalPanel = document.querySelector(".approval-panel");
-let reportPanel = document.querySelector(".report-panel");
+
 function displaydata() {
-  for (let j = 0; j <= dataArray.length; j++) {
-    if (j < 3) {
-      if (dataArray[j].menuId != dataArray[j].parentID) {
-        let sp = document.createElement("p");
-        sp.innerText = dataArray[j].menuName;
-        console.log(sp.innerText);
-        approvalPanel.appendChild(sp);
-      }
-    } else {
-      if (dataArray[j].menuId != dataArray[j].parentID) {
-        let sp = document.createElement("p");
-        sp.innerText = dataArray[j].menuName;
-        console.log(sp.innerText);
-        reportPanel.appendChild(sp);
-      }
-    }
-    if (dataArray[j].menuId == dataArray[j].parentID)
-      document.querySelector(".test-menu" + j).innerText =
-        dataArray[j].menuName;
-  }
-  document.querySelector(".menu-approval").innerText = dataArray[0].menuName;
-  document.querySelector(".report-menu").innerHTML = dataArray[3].menuName;
-}
-
-function done() {
+  let side_parent = document.getElementById("side-parent");
   for (let i = 0; i < dataArray.length; i++) {
-    console.log(dataArray[i].pageName);
+    if (dataArray[i].menuId == dataArray[i].parentID) {
+      let btn = document.createElement("button");
+      btn.className = "accordion";
+      let fspan = document.createElement("span");
+      let ficon = document.createElement("i");
+      ficon.className = "fa-solid fa-check-to-slot icon-accordion";
+      let sspan = document.createElement("span");
+      sspan.className = "menu-approval test-menu0";
+      sspan.innerText = dataArray[i].menuName;
+      let drop_icon = document.createElement("i");
+      drop_icon.className = "fa-solid fa-angle-down icon-down";
+
+      fspan.appendChild(ficon);
+      fspan.appendChild(sspan);
+
+      btn.appendChild(fspan);
+      btn.appendChild(drop_icon);
+
+      side_parent.appendChild(btn);
+
+      //  for child
+      let child_div = document.createElement("div");
+      child_div.className = "panel";
+
+      for (let j = i + 1; j < dataArray.length; j++) {
+        if (dataArray[j].parentID == dataArray[i].menuId) {
+          let p = document.createElement("p");
+
+          let a = document.createElement("a");
+          a.innerText = dataArray[j].menuName;
+          p.appendChild(a);
+          child_div.appendChild(p);
+        }
+      }
+      side_parent.appendChild(child_div);
+    }
+  }
+  console.log(side_parent);
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    console.log(i);
+    acc[i].addEventListener("click", function () {
+      /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+      this.classList.toggle("active");
+
+      /* Toggle between hiding and showing the active panel */
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        console.log("NONE");
+        panel.style.display = "none";
+      } else {
+        console.log("block");
+        panel.style.display = "block";
+      }
+    });
   }
 }
 
-// document.querySelector(".menu-approval").innerHTML = dataArray[0].pageName;
+// chart using vanilla js
 
-var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-var yValues = [55, 49, 44, 24, 18];
-var barColors = ["#C9BBCF", "#B4E4FF", "#FFADAD", "#FFE9AE", "#7DB9B6"];
-
-new Chart("myChart", {
-  type: "bar",
-  data: {
-    labels: xValues,
-
-    datasets: [
-      {
-        backgroundColor: barColors,
-        data: yValues,
-      },
-    ],
-  },
-  options: {
-    legend: { display: false },
-    title: {
-      display: true,
-      text: "World Wine Production 2018",
+//chart data
+var chartjson = {
+  title: "Acivitics of the employee",
+  data: [
+    {
+      name: "Kerry",
+      score: 20,
     },
-  },
-});
+    {
+      name: "Teegan",
+      score: 73,
+    },
+    {
+      name: "Jamalia",
+      score: 20,
+    },
+    {
+      name: "Quincy",
+      score: 89,
+    },
+    {
+      name: "Darryl",
+      score: 24,
+    },
+    {
+      name: "Jescie",
+      score: 86,
+    },
+    {
+      name: "Quemby",
+      score: 46,
+    },
+    {
+      name: "McKenzie",
+      score: 71,
+    },
+  ],
+  xtitle: "Secured Marks",
+  ytitle: "Marks",
+  ymax: 100,
+  ykey: "score",
+  xkey: "name",
+  prefix: "%",
+};
 
+//chart colors
+var colors = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+];
+
+//constants
+var TROW = "tr",
+  TDATA = "td";
+
+var chart = document.createElement("div");
+//create the chart canvas
+var barchart = document.createElement("table");
+//create the title row
+var titlerow = document.createElement(TROW);
+//create the title data
+var titledata = document.createElement(TDATA);
+//make the colspan to number of records
+titledata.setAttribute("colspan", chartjson.data.length + 1);
+titledata.setAttribute("class", "charttitle");
+titledata.innerText = chartjson.title;
+titlerow.appendChild(titledata);
+barchart.appendChild(titlerow);
+chart.appendChild(barchart);
+
+//create the bar row
+var barrow = document.createElement(TROW);
+
+//lets add data to the chart
+for (var i = 0; i < chartjson.data.length; i++) {
+  barrow.setAttribute("class", "bars");
+  var prefix = chartjson.prefix || "";
+  //create the bar data
+  var bardata = document.createElement(TDATA);
+  var bar = document.createElement("div");
+  bar.setAttribute("class", colors[i]);
+  bar.style.height = chartjson.data[i][chartjson.ykey] + prefix;
+  bardata.innerText = chartjson.data[i][chartjson.ykey] + prefix;
+  bardata.appendChild(bar);
+  barrow.appendChild(bardata);
+}
+
+//create legends
+var legendrow = document.createElement(TROW);
+var legend = document.createElement(TDATA);
+legend.setAttribute("class", "legend");
+legend.setAttribute("colspan", chartjson.data.length);
+
+//add legend data
+for (var i = 0; i < chartjson.data.length; i++) {
+  var legbox = document.createElement("span");
+  legbox.setAttribute("class", "legbox");
+  var barname = document.createElement("span");
+  barname.setAttribute("class", colors[i] + " xaxisname");
+  var bartext = document.createElement("span");
+  bartext.innerText = chartjson.data[i][chartjson.xkey];
+  legbox.appendChild(barname);
+  legbox.appendChild(bartext);
+  legend.appendChild(legbox);
+}
+barrow.appendChild(legend);
+barchart.appendChild(barrow);
+barchart.appendChild(legendrow);
+chart.appendChild(barchart);
+document.getElementById("chart").innerHTML = chart.outerHTML;
+
+//chart
+// var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+// var yValues = [55, 49, 44, 24, 18];
+// var barColors = ["#C9BBCF", "#B4E4FF", "#FFADAD", "#FFE9AE", "#7DB9B6"];
+
+// new Chart("myChart", {
+//   type: "bar",
+//   data: {
+//     labels: xValues,
+
+//     datasets: [
+//       {
+//         backgroundColor: barColors,
+//         data: yValues,
+//       },
+//     ],
+//   },
+//   options: {
+//     legend: { display: false },
+//     title: {
+//       display: true,
+//       text: "World Wine Production 2018",
+//     },
+//   },
+// });
+
+//on click button
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
+  console.log(i);
   acc[i].addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
